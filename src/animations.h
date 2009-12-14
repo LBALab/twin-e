b0ef749593160347e7b75e6823119c608d9f5fa4
Alexandre Fontoura <xesfnet@gmail.com>
2009-12-14 00:06:18 +0000

    * SVN props

--- src/animations.h
+++ src/animations.h
@@ -1,4 +1,4 @@
-/** @file animations.c
+/** @file animations.h
 	@brief
 	This file contains 3D actors animations routines
 	
@@ -20,6 +20,9 @@
 	You should have received a copy of the GNU General Public License \n
 	along with this program; if not, write to the Free Software \n
 	Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+	$URL$
+	$Id$
 */
 
 #ifndef ANIMATIONS_H
