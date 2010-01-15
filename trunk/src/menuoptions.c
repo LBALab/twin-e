9e6e66ffcb71566d45af5efb8fe16b73d446ff23
alexandrefontoura <alexandrefontoura@be2b35ff-e2fa-810a-e2f8-55846f3ca3d9>
2010-01-15 00:10:25 +0000

    Few fixes. Still wrong main palette set after showing intro screens.

--- trunk/src/menuoptions.c
+++ trunk/src/menuoptions.c
@@ -34,6 +34,8 @@
 #include "text.h"
 #include "gamestate.h"
 #include "music.h"
+#include "keyboard.h"
+#include "movies.h"
 
 void intro_text_clip_full() { // newGame2
 	dialTextBoxLeft = 8;
@@ -75,6 +77,36 @@ void new_game() {
 	draw_text_fullscreen(150);
 	read_keys();
 
+	if (skipIntro != 1) {
+		// intro screen 1 - twinsun
+		load_image( RESSHQR_INTROSCREEN2IMG, 1 );
+		draw_text_fullscreen(151);
+		read_keys();
+
+		if (skipIntro != 1) {
+			load_image( RESSHQR_INTROSCREEN3IMG, 1 );
+			draw_text_fullscreen(152);
+		}
+	}
+
+	newGameVar5 = 0;
+	intro_text_clip_small();
+	newGameVar4 = 1;
+
+	fade_2_black(paletteRGBACustom);
+	clear_screen();
+	flip();
+	
+	play_midi_music(1, 0);
+	play_movie(FLA_INTROD);
+
+	clear_screen();
+	flip();
+
+	convert_pal_2_RGBA(palette, paletteRGBA);
+	set_palette(paletteRGBA);
+
+	cfgfile.FlagDisplayText = tmpFlagDisplayText;
 }
 
 /** Main menu new game options */
--- trunk/src/text.h
+++ trunk/src/text.h
@@ -103,6 +103,8 @@ void draw_text(int32 x, int32 y, int8 *dialogue);
 	@param dialogue ascii text to display */
 int32 get_text_size(int8 *dialogue);
 
+void draw_text_fullscreen(int32 index);
+
 void set_font(uint8 *font, int32 spaceBetween, int32 charSpace);
 
 /** Set font type parameters
