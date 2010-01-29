3ef2f93c2a2d85c0f56b054ce3c871917eb6f56f
Alexandre Fontoura <xesfnet@gmail.com>
2010-01-29 23:46:36 +0000

    Fix related to issue #7

--- src/scene.c
+++ src/scene.c
@@ -112,14 +112,12 @@ void load_scene() {
 
 	alphaLight = *((uint16*)localScene);
 	localScene += 2;
-
-	// FIXME: Workaround to fix lighting issue - not using proper dark light
-	if (alphaLight < 896) {
-		alphaLight = 896;
-	}
-	
 	betaLight = *((uint16*)localScene);
 	localScene += 2;
+
+	// FIXME: Workaround to fix lighting issue - not using proper dark light
+	alphaLight = 896;
+	betaLight = 950;
 
 	sampleAmbiance[0] = *((uint16*)localScene);
 	localScene += 2;
