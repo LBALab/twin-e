b18e4dafe928ba54c634d5b07a7d2c0d30b2ee80
alexandrefontoura@gmail.com <alexandrefontoura@gmail.com@be2b35ff-e2fa-810a-e2f8-55846f3ca3d9>
2013-04-11 21:46:30 +0000

    Fix build mingw

--- trunk/dists/mingw_build.cmd
+++ trunk/dists/mingw_build.cmd
@@ -1,2 +1,4 @@
-cd /d "C:\Programas\MinGW\bin"
-mingw32-make.exe --directory=..\src --makefile=Makefile.mingw
+rem cd /d "C:\Program Files\MinGW\bin"
+"C:\Program Files\MinGW\bin\mingw32-make.exe" --directory=..\src --makefile=Makefile.mingw clean
+"C:\Program Files\MinGW\bin\mingw32-make.exe" --directory=..\src --makefile=Makefile.mingw
+pause
