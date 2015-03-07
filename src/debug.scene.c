a4d6f1de535d3871840019a529d84e5b2bf965de
felipe.sanches@gmail.com <felipe.sanches@gmail.com>
2015-03-07 16:12:59 +0000

    cleanup: removing unnecessary "\n" from the licensing headers.

--- src/debug.scene.c
+++ src/debug.scene.c
@@ -4,26 +4,23 @@
 
 	TwinEngine: a Little Big Adventure engine
 	
-	Copyright (C) 2013 The TwinEngine team \n
-	Copyright (C) 2008-2013 Prequengine team \n
-	Copyright (C) 2002-2007 The TwinEngine team \n
+	Copyright (C) 2013 The TwinEngine team
+	Copyright (C) 2008-2013 Prequengine team
+	Copyright (C) 2002-2007 The TwinEngine team
 
-	This program is free software; you can redistribute it and/or \n
-	modify it under the terms of the GNU General Public License \n
-	as published by the Free Software Foundation; either version 2 \n
-	of the License, or (at your option) any later version. \n
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
 
-	This program is distributed in the hope that it will be useful, \n
-	but WITHOUT ANY WARRANTY; without even the implied warranty of \n
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the \n
-	GNU General Public License for more details. \n
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-	You should have received a copy of the GNU General Public License \n
-	along with this program; if not, write to the Free Software \n
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
 	Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-
-	$URL$
-	$Id$
 */
 
 #include "debug.scene.h"
